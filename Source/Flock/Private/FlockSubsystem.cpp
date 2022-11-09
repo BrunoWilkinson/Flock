86b85642466bacade2a3cc233413e79351f61d87
BrunoWilkinson <bruno.wilkinson@gmail.com>
2022-11-09 18:35:04 +0000

    feat: add delegate and delegate handles

--- Source/Flock/Private/FlockSubsystem.cpp
+++ Source/Flock/Private/FlockSubsystem.cpp
@@ -4,10 +4,55 @@
 #include "FlockSubsystem.h"
 #include "OnlineSubsystem.h"
 
-UFlockSubsystem::UFlockSubsystem()
+UFlockSubsystem::UFlockSubsystem() :
+CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete)),
+FindSessionCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionComplete)),
+JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete)),
+DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete)),
+StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
 {
 	if (const IOnlineSubsystem* OnlineSubsystem = IOnlineSubsystem::Get())
 	{
 		SessionInterface = OnlineSubsystem->GetSessionInterface();
 	}
+}
+
+void UFlockSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+}
+
+void UFlockSubsystem::FindSession(int32 MaxSearchResults)
+{
+}
+
+void UFlockSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
+{
+}
+
+void UFlockSubsystem::DestroySession()
+{
+}
+
+void UFlockSubsystem::StartSession()
+{
+}
+
+void UFlockSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccesful)
+{
+}
+
+void UFlockSubsystem::OnFindSessionComplete(bool bWasSuccessful)
+{
+}
+
+void UFlockSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UFlockSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccesful)
+{
+}
+
+void UFlockSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccesful)
+{
 }
--- Source/Flock/Public/FlockSubsystem.h
+++ Source/Flock/Public/FlockSubsystem.h
@@ -17,9 +17,30 @@ class FLOCK_API UFlockSubsystem : public UGameInstanceSubsystem
 
 public:
 	UFlockSubsystem();
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+	void FindSession(int32 MaxSearchResults);
+	void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+	void DestroySession();
+	void StartSession();
 	
 protected:
-
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccesful);
+	void OnFindSessionComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccesful);
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccesful);
+	
 private:
 	IOnlineSessionPtr SessionInterface;
+
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	FOnFindSessionsCompleteDelegate FindSessionCompleteDelegate;
+	FDelegateHandle FindSessionCompleteDelegateHandle;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
 };
